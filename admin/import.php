cad0cb1ed168af777ce9d79400b44146cc832561
Lukex02 <minh.huynhba2004@hcmut.edu.vn>
2025-04-09 16:18:35 +0700

    change old php syntax

--- admin/import.php
+++ admin/import.php
@@ -5,9 +5,9 @@ $password = "";             // Mặc định không có mật khẩu
 $dbname = "slap";     // Tên database đã tạo
 
 // Kết nối MySQL
-$conn = new mysqli(hostname: $servername, username: $username, password: $password, database: $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Kiểm tra kết nối
 if ($conn->connect_error) {
-    die("Kết nối thất bại: ". $conn->connect_error);
+    die("Kết nối thất bại: " . $conn->connect_error);
 }
