d1ea157053f29fd1b08182d0ab0f724b71a0663f
Lukex02 <minh.huynhba2004@hcmut.edu.vn>
2025-04-13 18:29:55 +0700

    add create slap database if not exist

--- data/slap_29_3_2025.sql
+++ data/slap_29_3_2025.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `slap`
 --
-
+CREATE DATABASE IF NOT EXISTS `slap` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `slap`;
 -- --------------------------------------------------------
 
 --
