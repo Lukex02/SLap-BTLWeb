22a98d5d8f6b9cf67e641b27799a25d4168036e0
Lukex02 <minh.huynhba2004@hcmut.edu.vn>
2025-04-29 22:00:08 +0700

    fix label color in chart

--- js/adminIndex.js
+++ js/adminIndex.js
@@ -1,87 +1,3 @@
-var optionsComments = {
-  series: [
-    {
-      name: "Số lượng tài khoản được thành lập",
-      data: [310, 800, 600, 430, 540, 340, 605, 805, 430, 540, 340, 605],
-    },
-  ],
-  chart: {
-    height: 80,
-    type: "area",
-    toolbar: {
-      show: false,
-    },
-  },
-  colors: ["#5350e9"],
-  stroke: {
-    width: 2,
-  },
-  grid: {
-    show: false,
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  xaxis: {
-    type: "datetime",
-    categories: [
-      "2018-09-19T00:00:00.000Z",
-      "2018-09-19T01:30:00.000Z",
-      "2018-09-19T02:30:00.000Z",
-      "2018-09-19T03:30:00.000Z",
-      "2018-09-19T04:30:00.000Z",
-      "2018-09-19T05:30:00.000Z",
-      "2018-09-19T06:30:00.000Z",
-      "2018-09-19T07:30:00.000Z",
-      "2018-09-19T08:30:00.000Z",
-      "2018-09-19T09:30:00.000Z",
-      "2018-09-19T10:30:00.000Z",
-      "2018-09-19T11:30:00.000Z",
-    ],
-    axisBorder: {
-      show: false,
-    },
-    axisTicks: {
-      show: false,
-    },
-    labels: {
-      show: false,
-    },
-  },
-  show: false,
-  yaxis: {
-    labels: {
-      show: false,
-    },
-  },
-  tooltip: {
-    x: {
-      format: "dd/MM/yy HH:mm",
-    },
-  },
-};
-
-// const optionsProducts = {
-// series: [70, 30],
-// labels: ["Male", "Female"],
-// colors: ["#435ebe", "#55c6e8"],
-// chart: {
-//   type: "donut",
-//   width: "100%",
-//   height: "350px",
-// },
-// legend: {
-//   position: "bottom",
-// },
-// plotOptions: {
-//   pie: {
-//     donut: {
-//       size: "30%",
-//     },
-//   },
-// },
-// };
-
 document.addEventListener("DOMContentLoaded", function () {
   fetch("/server/session.php")
     .then((response) => response.json())
@@ -234,6 +150,9 @@ function drawChartComments() {
           type: "category",
           title: {
             text: "Thời gian",
+            style: {
+              color: "rgb(155, 170, 201)",
+            },
           },
         },
         yaxis: {
@@ -305,6 +224,9 @@ function drawChartArticles() {
           type: "category",
           title: {
             text: "Thời gian",
+            style: {
+              color: "rgb(155, 170, 201)",
+            },
           },
         },
         yaxis: {
