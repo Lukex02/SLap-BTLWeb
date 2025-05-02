6cf2205b3d7ed6d94e73592878e57e1611d2472b
Lukex02 <minh.huynhba2004@hcmut.edu.vn>
2025-05-02 18:15:56 +0700

    rename

--- admin/ArticleAdmin.php
+++ admin/ArticleAdmin.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Product - SLap Admin Dashboard</title>
+    <title>Article - SLap Admin Dashboard</title>
 
     <link rel="shortcut icon" href="./assets/compiled/svg/favicon.svg" type="image/x-icon" />
     <link rel="shortcut icon"
