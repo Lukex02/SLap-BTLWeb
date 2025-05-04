62ea1fe07a0c87303b6d0e89715ef100638907cb
Lukex02 <minh.huynhba2004@hcmut.edu.vn>
2025-05-04 12:09:20 +0700

    revert article pages from main

--- page/article.php
+++ page/article.php
@@ -51,6 +51,7 @@
     function displayArticle({ article, comments }) {
       const articleDetail = document.getElementById("article");
       const title = document.title = article.title;
+
       articleDetail.innerHTML = `
           <header class="article-header">
             <h1>${article.title}</h1>
@@ -59,7 +60,9 @@
             <p><strong>Tác giả:</strong> ${article.author_name}</p>
             <p><strong>Ngày đăng:</strong> ${new Date(article.published_at).toLocaleDateString()}</p>
             <p><strong>Chuyên mục:</strong> ${article.category}</p>
-            <p><strong>Lượt thích:</strong> ${article.likes}</p>
+            <p><strong>Lượt thích:</strong> ${article.likes}
+              <a href="#" id="like"><i class="bi bi-hand-thumbs-up-fill"></i></a>
+            </p>
           </div>
           <div class="article-content">
             ${article.content}
@@ -70,29 +73,107 @@
                 <h3>Bình luận (${article.comments_count})</h3>
               </div>
               <div class="card-body">
-                <div class="d-flex mb-4">
+                <form class="d-flex mb-3 visually-hidden" id="comment-form">
                   <div class="d-flex flex-column justify-content-start me-2">
-                    <img src="/pic/def_author_avatar.png" alt="Avatar của bạn" class="rounded-circle comment-avatar">
+                    <img alt="Avatar của bạn" class="rounded-circle comment-avatar" id="userAvatar">
                     <span class="text-center">Bạn</span>
                   </div>
-                  <textarea class="form-control"  id="exampleFormControlTextarea1" rows="3"></textarea>
-                  <button type="button" class="btn btn-primary">Primary</button>
+                  <div class="d-flex flex-column w-100 gap-3">
+                    <div class="form-floating">
+                      <textarea class="form-control" placeholder="Leave a comment here" id="comment-content"></textarea>
+                      <label for="comment-content">Comments</label>
+                    </div>
+                    <div class="d-flex justify-content-end">
+                      <button type="submit" class="btn btn-primary">Đăng</button>
+                    </div>
+                  </div>
+                </form>
+                <div class="d-flex justify-content-end" id="signInToCommentBtn">
+                  <a role="button" href="/signin" class="btn btn-primary">Đăng nhập để bình luận</a>
                 </div>
                 <ul class="comment-list list-group list-group-flush">
-                ${comments.map(comment => `
-                <li class="comment list-group-item d-flex py-3">
-                  <img src="${comment.commenter_avatar}" alt="${comment.commenter_name}" class="rounded-circle comment-avatar me-2">
-                  <div class="comment-body">
-                    <p class="mb-1"><strong>${comment.commenter_name}</strong>: ${comment.content}</p>
-                    <small>${new Date(comment.created_at).toLocaleString()}</small>
-                  </div>
-                </li>
-              `).join("")}
+                  ${comments.map(comment => `
+                  <li class="comment list-group-item d-flex py-3">
+                    <img src="${comment.commenter_avatar}" alt="${comment.commenter_name}" class="rounded-circle comment-avatar me-2">
+                    <div class="comment-body">
+                      <p class="mb-1"><strong>${comment.commenter_name}</strong>: ${comment.content}</p>
+                      <small>${new Date(comment.created_at).toLocaleString()}</small>
+                    </div>
+                  </li>
+                `).join("")}
                 </ul>
               </div>
             </div>
           </footer>
           `;
+
+      document.getElementById("like").addEventListener("click", function () {
+        const param = new URLSearchParams()
+        param.append("id", article.id)
+        fetch("/server/likeArticle.php", {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded' // Quan trọng
+          },
+          body: param.toString(),
+        })
+          .then((response) => response.text())
+          .then((data) => {
+            alert(data)
+            window.location.reload()
+          })
+      })
+
+      fetch("/server/session.php")
+        .then((response) => response.json())
+        .then((data) => {
+          if (data.success) {
+            const commentForm = document.getElementById("comment-form")
+            commentForm.classList.remove("visually-hidden");
+            const signInToCommentBtn = document.getElementById("signInToCommentBtn")
+            signInToCommentBtn.classList.add("visually-hidden");
+            document.getElementById("userAvatar").src = data.avatar // Add user avatar
+
+            commentForm.addEventListener("submit", (e) => {
+              e.preventDefault();
+              const commentContent = document.getElementById("comment-content").value
+              if (commentContent.length <= 0) {
+                alert("Bạn chưa nhập nội dung bình luận")
+                return
+              }
+
+              // Lấy dữ liệu từ form
+              const commentFormData = {
+                content: commentContent,
+                article_id: article.id,
+              };
+              fetch("/server/setComment.php", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                  content: commentFormData.content,
+                  article_id: commentFormData.article_id,
+                }),
+              })
+                .then((response) => response.json())
+                .then((data) => {
+                  if (data.success) {
+                    alert(data.message)
+                    window.location.reload();
+                  } else {
+                    console.log(data.message)
+                  }
+                })
+                .catch((err) => console.error(err))
+                .catch((err) => console.error(err))
+            })
+          }
+        })
+        .catch((error) => console.error("Lỗi tải dữ liệu:", error));
+
+
     }
   </script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
