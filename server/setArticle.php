79638f4e426c7e554264e31a5febf18b50c788f3
Huỳnh Bá Minh <165473083+Lukex02@users.noreply.github.com>
2025-05-04 12:58:33 +0700

    Update setArticle.php

--- server/setArticle.php
+++ server/setArticle.php
@@ -64,8 +64,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (isset($_FILES['thumbnail']) && $_FILES['thumbnail']['error'] === UPLOAD_ERR_OK) {
     $fileTmpPath = $_FILES['thumbnail']['tmp_name'];
     $fileName = $_FILES['thumbnail']['name'];
-    // $fileSize = $_FILES['thumbnail']['size'];
-    // $fileType = $_FILES['thumbnail']['type'];
 
     // Lấy đuôi file
     $fileExtension = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
@@ -130,7 +128,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       $stmt->bind_param($types, $title, $slug, $authorName, $category, $tags, $updatedAt, $content, $excerpt, $thumbnail, $id);
     } else {
       // echo "New ";
-      $stmt->bind_param($types, $title, $slug, $authorName, $authorAvatar, $category, $tags, $publishedAt, $updatedAt, $content, $excerpt, $thumbnail, $views, $likes);
+      $stmt->bind_param($types, $title, $slug, $authorName, $authorAvatar, $category, $tags, $publishedAt, $updatedAt, $content, $excerpt, $thumbnail, $likes);
     }
     if ($stmt->execute()) {
       echo "Nội dung đã được lưu thành công vào cơ sở dữ liệu.<br>";
