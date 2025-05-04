73023fe99a527d230035a4a2033d7eb003c129b6
Huỳnh Bá Minh <165473083+Lukex02@users.noreply.github.com>
2025-05-04 12:30:01 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -14,8 +14,10 @@
   <link rel="stylesheet" href="./assets/compiled/css/app.css" />
   <link rel="stylesheet" href="./assets/compiled/css/app-dark.css" />
   <link rel="stylesheet" href="./assets/compiled/css/iconly.css" />
+  <script src="assets/extensions/apexcharts/apexcharts.min.js"></script>
   <script src="assets/extensions/sweetalert2/sweetalert2.min.js"></script>
   <script src="/js/adminCheck.js"></script>
+  <script src="/js/adminIndex.js"></script>
 </head>
 
 
@@ -43,12 +45,12 @@
                     <div class="row">
                       <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
                         <div class="stats-icon purple mb-2">
-                          <i class="iconly-boldShow"></i>
+                          <i class="iconly-boldPaper"></i>
                         </div>
                       </div>
                       <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                        <h6 class="text-muted font-semibold">Profile Views</h6>
-                        <h6 class="font-extrabold mb-0">112.000</h6>
+                        <h6 class="text-muted font-semibold">Số lượng bài viết</h6>
+                        <h6 class="font-extrabold mb-0" id="article-num"></h6>
                       </div>
                     </div>
                   </div>
@@ -60,12 +62,12 @@
                     <div class="row">
                       <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
                         <div class="stats-icon blue mb-2">
-                          <i class="iconly-boldProfile"></i>
+                          <i class="iconly-boldChat"></i>
                         </div>
                       </div>
                       <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                        <h6 class="text-muted font-semibold">Followers</h6>
-                        <h6 class="font-extrabold mb-0">183.000</h6>
+                        <h6 class="text-muted font-semibold">Số lượng comment</h6>
+                        <h6 class="font-extrabold mb-0" id="comment-num"></h6>
                       </div>
                     </div>
                   </div>
@@ -77,12 +79,12 @@
                     <div class="row">
                       <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
                         <div class="stats-icon green mb-2">
-                          <i class="iconly-boldAdd-User"></i>
+                          <i class="iconly-boldUser"></i>
                         </div>
                       </div>
                       <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                        <h6 class="text-muted font-semibold">Following</h6>
-                        <h6 class="font-extrabold mb-0">80.000</h6>
+                        <h6 class="text-muted font-semibold">Số lượng tài khoản</h6>
+                        <h6 class="font-extrabold mb-0" id="account-num"></h6>
                       </div>
                     </div>
                   </div>
@@ -94,12 +96,12 @@
                     <div class="row">
                       <div class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-start">
                         <div class="stats-icon red mb-2">
-                          <i class="iconly-boldBookmark"></i>
+                          <i class="iconly-boldBuy"></i>
                         </div>
                       </div>
                       <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                        <h6 class="text-muted font-semibold">Saved Post</h6>
-                        <h6 class="font-extrabold mb-0">112</h6>
+                        <h6 class="text-muted font-semibold">Số lượng sản phẩm</h6>
+                        <h6 class="font-extrabold mb-0" id="product-num"></h6>
                       </div>
                     </div>
                   </div>
@@ -116,49 +118,27 @@
                     <div class="row">
                       <div class="col-7">
                         <div class="d-flex align-items-center">
-                          <svg class="bi text-primary" width="32" height="32" fill="blue" style="width: 10px">
+                          <svg class="bi text-success" width="32" height="32" fill="#FB773C" style="width: 10px">
                             <use xlink:href="assets/static/images/bootstrap-icons.svg#circle-fill" />
                           </svg>
-                          <h5 class="mb-0 ms-3">TPHCM</h5>
+                          <h5 class="mb-0 ms-3">Bài viết</h5>
                         </div>
                       </div>
-                      <div class="col-5">
-                        <h5 class="mb-0 text-end">862</h5>
-                      </div>
                       <div class="col-12">
-                        <div id="chart-europe"></div>
+                        <div id="chart-articles"></div>
                       </div>
                     </div>
                     <div class="row">
                       <div class="col-7">
                         <div class="d-flex align-items-center">
-                          <svg class="bi text-success" width="32" height="32" fill="blue" style="width: 10px">
+                          <svg class="bi text-danger" width="32" height="32" fill="#A0C878" style="width: 10px">
                             <use xlink:href="assets/static/images/bootstrap-icons.svg#circle-fill" />
                           </svg>
-                          <h5 class="mb-0 ms-3">Hà Nội</h5>
+                          <h5 class="mb-0 ms-3">Bình luận</h5>
                         </div>
                       </div>
-                      <div class="col-5">
-                        <h5 class="mb-0 text-end">375</h5>
-                      </div>
                       <div class="col-12">
-                        <div id="chart-america"></div>
-                      </div>
-                    </div>
-                    <div class="row">
-                      <div class="col-7">
-                        <div class="d-flex align-items-center">
-                          <svg class="bi text-danger" width="32" height="32" fill="blue" style="width: 10px">
-                            <use xlink:href="assets/static/images/bootstrap-icons.svg#circle-fill" />
-                          </svg>
-                          <h5 class="mb-0 ms-3">Đà Nẵng</h5>
-                        </div>
-                      </div>
-                      <div class="col-5">
-                        <h5 class="mb-0 text-end">1025</h5>
-                      </div>
-                      <div class="col-12">
-                        <div id="chart-indonesia"></div>
+                        <div id="chart-comments"></div>
                       </div>
                     </div>
                   </div>
@@ -167,46 +147,19 @@
               <div class="col-12">
                 <div class="card">
                   <div class="card-header">
-                    <h4>Latest Comments</h4>
+                    <h4>Comments gần đây</h4>
                   </div>
                   <div class="card-body">
                     <div class="table-responsive">
                       <table class="table table-hover table-lg">
                         <thead>
                           <tr>
-                            <th>Name</th>
+                            <th>Tên</th>
                             <th>Comment</th>
+                            <th>Từ bài viết</th>
                           </tr>
                         </thead>
-                        <tbody>
-                          <tr>
-                            <td class="col-3">
-                              <div class="d-flex align-items-center">
-                                <div class="avatar avatar-md">
-                                  <img src="./assets/compiled/jpg/5.jpg" />
-                                </div>
-                                <p class="font-bold ms-3 mb-0">Si Cantik</p>
-                              </div>
-                            </td>
-                            <td class="col-auto">
-                              <p class="mb-0">Congratulations on your graduation!</p>
-                            </td>
-                          </tr>
-                          <tr>
-                            <td class="col-3">
-                              <div class="d-flex align-items-center">
-                                <div class="avatar avatar-md">
-                                  <img src="./assets/compiled/jpg/2.jpg" />
-                                </div>
-                                <p class="font-bold ms-3 mb-0">Si Ganteng</p>
-                              </div>
-                            </td>
-                            <td class="col-auto">
-                              <p class="mb-0">
-                                Wow amazing design! Can you make another tutorial for this design?
-                              </p>
-                            </td>
-                          </tr>
+                        <tbody id="comment-section">
                         </tbody>
                       </table>
                     </div>
@@ -215,65 +168,37 @@
               </div>
             </div>
           </div>
+          <!-- Cột phải -->
           <div class="col-12 col-lg-3">
-            <div class="card">
+            <div class="card" id="user-info">
               <div class="card-body py-4 px-4">
                 <div class="d-flex align-items-center">
                   <div class="avatar avatar-xl">
                     <img src="./assets/compiled/jpg/1.jpg" alt="Face 1" />
                   </div>
                   <div class="ms-3 name">
-                    <h5 class="font-bold">John Duck</h5>
-                    <h6 class="text-muted mb-0">@johnducky</h6>
+                    <h5 class="font-bold" id="username"></h5>
+                    <h6 class="text-muted mb-0" id="usertag"></h6>
                   </div>
+                </div>
+              </div>
+            </div>
+            <div class="card" id="user-info">
+              <div class="card-body py-4 px-4">
+                <div class="d-flex align-items-center">
+                  <a href="/" role="button" class="btn btn-warning w-100 d-flex justify-content-center">
+                    <i class="bi bi-house-fill me-2"></i>
+                    Về trang chủ
+                  </a>
                 </div>
               </div>
             </div>
             <div class="card">
               <div class="card-header">
-                <h4>Recent Messages</h4>
-              </div>
-              <div class="card-content pb-4">
-                <div class="recent-message d-flex px-4 py-3">
-                  <div class="avatar avatar-lg">
-                    <img src="./assets/compiled/jpg/4.jpg" />
-                  </div>
-                  <div class="name ms-4">
-                    <h5 class="mb-1">Hank Schrader</h5>
-                    <h6 class="text-muted mb-0">@johnducky</h6>
-                  </div>
-                </div>
-                <div class="recent-message d-flex px-4 py-3">
-                  <div class="avatar avatar-lg">
-                    <img src="./assets/compiled/jpg/5.jpg" />
-                  </div>
-                  <div class="name ms-4">
-                    <h5 class="mb-1">Dean Winchester</h5>
-                    <h6 class="text-muted mb-0">@imdean</h6>
-                  </div>
-                </div>
-                <div class="recent-message d-flex px-4 py-3">
-                  <div class="avatar avatar-lg">
-                    <img src="./assets/compiled/jpg/1.jpg" />
-                  </div>
-                  <div class="name ms-4">
-                    <h5 class="mb-1">John Dodol</h5>
-                    <h6 class="text-muted mb-0">@dodoljohn</h6>
-                  </div>
-                </div>
-                <div class="px-4">
-                  <button class="btn btn-block btn-xl btn-outline-primary font-bold mt-3">
-                    Start Conversation
-                  </button>
-                </div>
-              </div>
-            </div>
-            <div class="card">
-              <div class="card-header">
-                <h4>Visitors Profile</h4>
+                <h4>Sản phẩm</h4>
               </div>
               <div class="card-body">
-                <div id="chart-visitors-profile"></div>
+                <div id="chart-products"></div>
               </div>
             </div>
           </div>
@@ -287,9 +212,6 @@
 
   <script src="assets/compiled/js/app.js"></script>
 
-  <!-- Need: Apexcharts -->
-  <script src="assets/extensions/apexcharts/apexcharts.min.js"></script>
-  <script src="assets/static/js/pages/dashboard.js"></script>
 </body>
 
 </html>
